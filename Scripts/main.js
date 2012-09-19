d594ed42e2eb0a52c6cd1d8a822c579870119c81
Salvatore Rinchiera <srinchiera@college.harvard.edu>
2012-09-19 19:24:20 -0400

    please dont break everything

--- Scripts/main.js
+++ Scripts/main.js
@@ -5,46 +5,17 @@ $('#searchResultsDiv').hide();
 $('#myStudyGroupsDiv').hide();
 $('#helpDiv').hide();
 
-$('#submitCreateGroup').click(function(e) {
-    var email = $('#email').val();  
-    var location = $('#location').val();  
-    var name = $('#fullname').val();  
-    var date = $('#date').val();  
-    var time = $('#time').val();  
-    myClasses.push({location: location, email: email, name: name, date: date, time: time});
-    $('#createGroupModal').modal('toggle');
-});
+/* Your code goes here 1*/
+
+
 
 var myClasses = []; 
 var classDict = {};
-classDict["cs50"] = {name:"Computer Science 50", description:"Rip phonebooks and other cool things to do with computer science."}
-classDict["cs61"] = {name:"Computer Science 61", description:"The coolest class you will ever take at Harvard University. Period."}
-classDict["stat110"] = {name:"Statistics 110", description:"lolwut, why am I taking a stats course if I'm a CS concentrator?"}
-$('#sidenavbar li').click(function(e) {
-      $('#navigationDivs').show();
-      $('#searchResults').hide();
 
-      var self = $(this);
-      if (!self.hasClass('active')) {
-        self.addClass('active');
-      }
-      self.siblings().removeClass('active');
+/* Populate class dicts here */
 
-      if (self.attr('id') == "myStudyGroups"){
-          populateMyGroups();
-      }
+/* Nav bar stuff here */
 
-      divId = (self.attr('id') + "Div");
-      div = $("#"+divId);
-
-
-      currentDiv.slideRightHide();
-      div.slideLeftShow();
-
-
-      currentDiv = div;
-      e.preventDefault();
-});
 
 $('#submitSearch').click(function(e) {
     var self = $(this);
@@ -98,7 +69,7 @@ jQuery.fn.extend({
 var populateMyGroups = function() {
     var html = "";
 
-    var attendButtons = "<div class='btn-group'><button id='attending' class='btn btn-success'>Attending</button><button id='notAttending' class='btn btn-error'>Not Attending</button></div>";
+	var attendButtons = "<div class='btn-group'><button id='attending' class='btn btn-success'>Attending</button><button id='notAttending' class='btn btn-error'>Not Attending</button></div>";
 
     for (var i in myClasses){
         html = html + "<div style='position:relative;left:130px;top:259px' class='modal'><div class='modal-header'><h2>omg how do we do this</h2></div><div class='modal-body'><p>Location: " + myClasses[i].location +"</p><p>Date: " + myClasses[i].date+ "</p><p>Time: "+myClasses[i].time+ "</p><p>Organizer: <a href=mailto:" + myClasses[i].email + ">"+myClasses[i].name+"</a></p>"+attendButtons+"</div>";
